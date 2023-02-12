a7caf4b9eebb3a5b8836a138987dc0ebf0b9e452
Dmitrian23 <dimonio1991@mail.ru>
2023-02-12 19:36:41 +0300

    Задача 041

---
+++ 041/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача_№41 Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+// int [] Array2;
+// Array2=new int[8];
+
+// Random random=new Random();
+// for(int i=0;i<Array2.Length;i++)
+// Array2[i]=random.Next(0,2);
+
+// for(int i=0;i<Array2.Length;i++)
+// System.Console.Write($"[ {Array2[i]} ]");
+// System.Console.WriteLine();
+
+
+int [] Array2=new int[] {1,0,1,0,1,0,1,0};
+for(int i=0;i<Array2.Length;i++)
+System.Console.Write($"[ {Array2[i]} ]");
+
+
+
