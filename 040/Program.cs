377030214d7817c32814e369b43199d28862d9b1
Dmitrian23 <dimonio1991@mail.ru>
2023-02-12 19:36:26 +0300

    Задача 040

---
+++ 040/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача_№40 Задать массив из 8 целых элементов и вывести их на экран
+
+int [] Array1;
+Array1=new int[8];
+
+Random random=new Random();
+for(int i=0;i<Array1.Length;i++)
+Array1[i]=random.Next(1,10);
+
+for(int i=0;i<Array1.Length;i++)
+System.Console.Write($"[ {Array1[i]} ]");
+System.Console.WriteLine();
+
